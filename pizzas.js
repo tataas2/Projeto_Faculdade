8f636e1bbda1bd2707ea0b0a0a792f67d5ab2b35
tataas2 <94724797+tataas2@users.noreply.github.com>
2024-06-02 22:46:23 -0300

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'MAXIMOFF',
+        img: 'images/pizza.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+      },
+      {
+        id: 2,
+        name: 'POR ASGARD!!',
+        img: 'images/pizza2.png',
+        price: [21.00, 24.00, 26.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+      },
+      {
+        id: 3,
+        name: 'COM GRANDES PODERES… ',
+        img: 'images/pizza3.png',
+        price: [23.00, 26.00, 28.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'A nossa grande responsabilidade é entregar a melhor pizza 5 queijos deste universo! O melhor do queijo mussarela, gouda, parmesão, gorgonzola e provolone em um só lugar.'
+      },
+      {
+        id: 4,
+        name: 'HULK ESMAGA!!',
+        img: 'images/pizza4.png',
+        price: [25.00, 28.00, 30.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Pizza de brócolis americano com queijo mussarela, bacon, cebola e azeitona roxa'
+      },
+      {
+        id: 5,
+        name: 'EU SOU GROOT',
+        img: 'images/pizza5.png',
+        price: [24.00, 27.00, 29.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'O melhor atum assado com palmito, catupiry, azeitonas, cebola e ovos em rodelas.     '
+      },
+      {
+        id: 6,
+        name: 'GÊNIO, BILIONÁRIO, PLAYBOY, FILANTROPO',
+        img: 'images/pizza6.png',
+        price: [30.00, 33.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'O mais legítimo Pepperoni acompanhado com bacon crispy e mussarela.'
+      },
+      {
+        id: 7,
+        name: 'ROMANOFF',
+        img: 'images/pizza7.png',
+        price: [27.00, 30.00, 32.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Pedaços de bananas cobertas com doce de leite, lascas de chocolate branco e canela em pó.'
+      },
+      {
+        id: 8,
+        name: 'GAMORA',
+        img: 'images/pizza8.png',
+        price: [30.00, 32.00, 35.00],
+        sizes: [
+          '6 fatias',
+          '8 fatias',
+          '12 fatias'
+        ],
+        description: 'Creme de leite, lascas de chocolate e morangos'
+      }
+];
